bc17d1d94fca8c9d6728b75184c601aee5250ae5
TerrenceBerg <Terrence@976-TUNA.com>
2025-04-08 13:56:27 -0700

    update location

--- src/resources/views/pwa.blade.php
+++ src/resources/views/pwa.blade.php
@@ -55,7 +55,7 @@
         // Register service worker after page load to ensure better performance
         if ('serviceWorker' in navigator) {
             window.addEventListener('load', function() {
-                navigator.serviceWorker.register('/serviceworker.js')
+                navigator.serviceWorker.register('/js/serviceworker.js')
                     .then(function(registration) {
                         //console.log('Service Worker registered with scope:', registration.scope);
                     })
